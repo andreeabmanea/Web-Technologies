ce11d393d5380ccbad9ad6fd98993fa25c4a1cfe
raresjunica <79074292+raresjunica@users.noreply.github.com>
2021-06-06 13:32:16 +0300

    Added phpmailer

--- PHP/backend/mailer/newOrder.php
+++ PHP/backend/mailer/newOrder.php
@@ -1,36 +1,60 @@
 <?php
-require 'PHPMailerAutoload.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer;
+global $mysql;
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'user@example.com';                 // SMTP username
-$mail->Password = 'secret';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-$mail->setFrom('from@example.com', 'Mailer');
-$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');
 
-$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+if (isset($_POST['name']))
+    $name = $_POST['name'];
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+if (isset($_POST['phone_number']))
+    $phone_number = $_POST['phone_number'];
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
+if (isset($_POST['ddate']))
+    $date = $_POST['ddate'];
+
+if (isset($_POST['dhour']))
+    $hour = $_POST['dhour'];
+
+$stmt_order = $mysql->prepare("select email from users where phone_number=$phone_number");
+$stmt_order->execute();
+$result_id = $stmt_order->get_result();
+$info_order = $result_id->fetch_assoc();
+$stmt_order->close();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = EMAIL;                     //SMTP username
+    $mail->Password   = PASS;                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom(EMAIL, 'localhost');
+    $mail->addAddress($result_id, $name);     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Order Update';
+    $mail->Body    = ' Your order has been updated!</b>';
+    $mail->AltBody = 'It will be delivered in ' . $date . ' at ' . $hour . '.';
+
+    $mail->send();
     echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
--- PHP/backend/mailer/updateOrder.php
+++ PHP/backend/mailer/updateOrder.php
@@ -1,13 +1,18 @@
 <?php
-$title = 'Client Account';
-require_once "../includes/header_for_accounts.php";
-require_once("../server/connection.php");
-require 'PHPMailerAutoload.php';
-require 'credentials.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 global $mysql;
 
-$mail = new PHPMailer;
+//Load Composer's autoloader
+
+require '../mailer/MAILER/vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 if (isset($_POST['name']))
     $name = $_POST['name'];
@@ -21,33 +26,35 @@ if (isset($_POST['ddate']))
 if (isset($_POST['dhour']))
     $hour = $_POST['dhour'];
 
-$stmt_order = $mysql->prepare("select email from users where phone_number=?");
-$stmt_order->bind_param('s', $phone_number);
+$stmt_order = $mysql->prepare("select email from users where phone_number=$phone_number");
 $stmt_order->execute();
 $result_id = $stmt_order->get_result();
 $info_order = $result_id->fetch_assoc();
 $stmt_order->close();
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jmvcourier@gmail.com';                     //SMTP username
+    $mail->Password   = 'jmvcourier12345678';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = EMAIL;                 // SMTP username
-$mail->Password = PASS;                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+    //Recipients
+    $mail->setFrom('jmvcourier@gmail.com', 'localhost');
+    $mail->addAddress($result_id, $name);     //Add a recipient
 
-$mail->setFrom(EMAIL, 'localhost    ');
-$mail->addAddress($info_order, $name);     // Add a recipient
-$mail->isHTML(true);                                  // Set email format to HTML
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Order Update';
+    $mail->Body    = ' Your order has been updated!</b>';
+    $mail->AltBody = 'It will be delivered in ' . $date . ' at ' . $hour . '.';
 
-$mail->Subject = 'Order Update';
-$mail->Body    = ' Your order has been updated!</b>';
-$mail->AltBody = 'It will be delivered in ' . $date . ' at ' . $hour . '.';
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
+    $mail->send();
     echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
